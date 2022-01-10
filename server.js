02144ea173d279679d43e7e2842dc18f87a6bd2d
Nathaniel Pather <nathaniel_pather@hotmail.com>
2022-01-10 17:15:42 +1000

    First commit

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+const mongoose = require('mongoose');
+app = express(),
+port = process.env.port || 3000;
+
+// moodel loading here
+Task = require('./api/models/ToDoListModel');
+bodyParser = require('body-parser');
+
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// importing route
+var routes = require('./api/routes/ToDoListRoutes');
+// register the route
+routes(app);
+
+// 
+app.use(function(req, res) {
+    res.status(404).send({
+        url: req.originalUrl + ' not found'
+    })
+})
+
+app.listen(port);
+
+console.log('ToDo List RESTful API server started on: ' + port);
